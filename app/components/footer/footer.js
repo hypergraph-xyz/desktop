fd9b3b57e27f74b6930d58ff558f1e40799eb0e7
Julian Gruber <julian@juliangruber.com>
2020-07-30 10:32:36 +0200

    Footer: Remove UpArrow. Closes #199

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,8 +1,6 @@
 import { shell } from 'electron'
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import Arrow from '../icons/arrow.svg'
-import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 import Anchor from '../anchor'
 
 const Container = styled.div`
@@ -18,37 +16,15 @@ const Title = styled.div`
   font-size: 2em;
   margin-bottom: 77px; /* TODO */
 `
-const UpArrow = styled(Arrow)`
-  position: absolute;
-  right: 2rem;
-  bottom: 2rem;
-  -webkit-app-region: no-drag;
-`
-const scrollToTop = () => {
-  document.documentElement.scrollTop = 0
-}
 
-const Footer = ({ title }) => {
-  const [upArrowVisible, setUpArrowVisible] = useState(false)
-
-  useScrollPosition(
-    ({ currPos: { y } }) => {
-      const isVisible = y < 48
-      if (isVisible !== upArrowVisible) setUpArrowVisible(isVisible)
-    },
-    [upArrowVisible]
-  )
-
-  return (
-    <Container>
-      <Title>{title}</Title>
-      Cooperatively made with 💜 in Berlin by{' '}
-      <Anchor onClick={() => shell.openExternal('https://libscie.org')}>
-        Liberate Science GmbH
-      </Anchor>
-      {upArrowVisible && <UpArrow onClick={scrollToTop} />}
-    </Container>
-  )
-}
+const Footer = ({ title }) => (
+  <Container>
+    <Title>{title}</Title>
+    Cooperatively made with 💜 in Berlin by{' '}
+    <Anchor onClick={() => shell.openExternal('https://libscie.org')}>
+      Liberate Science GmbH
+    </Anchor>
+  </Container>
+)
 
 export default Footer
