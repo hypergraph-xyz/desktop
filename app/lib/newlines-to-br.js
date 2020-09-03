cd34c8e8fde8bd1117c04a2158fbaa75b6175098
Julian Gruber <julian@juliangruber.com>
2020-09-03 08:32:11 +0200

    newlines-to-br: Fix edge case in key construction

--- app/lib/newlines-to-br.js
+++ app/lib/newlines-to-br.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 
 export default text =>
   text.split(/\r?\n/).map((line, i) => (
-    <Fragment key={String(i)}>
+    <Fragment key={`${String(i)}${line}`}>
       {line}
       <br />
     </Fragment>
