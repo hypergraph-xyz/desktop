0f944ef0f0c189d04566e08092ede14374fe946c
Julian Gruber <julian@juliangruber.com>
2020-09-23 09:31:26 +0200

    drafts: only list own drafts

--- app/components/drafts/drafts.js
+++ app/components/drafts/drafts.js
@@ -20,6 +20,7 @@ export default ({ p2p }) => {
       ])
       setHasRegisteredContent(profile.rawJSON.contents.length)
       const drafts = contents
+        .filter(content => content.rawJSON.authors.includes(encode(profileUrl)))
         .filter(content => !isContentRegistered(content, profile))
         .sort(sort)
       setDrafts(drafts)
