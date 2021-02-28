edc5c863ea07dea5619d2c00a57e9bc916a0660f
Chris Hartgerink <chris@libscie.org>
2021-02-28 19:37:33 +0100

    Update welcome.js (#382)

--- app/components/welcome/welcome.js
+++ app/components/welcome/welcome.js
@@ -319,7 +319,7 @@ const dialogs = [
           . Sign up for our{' '}
           <Anchor href='https://www.libscie.org/#newsletter'>newsletter</Anchor>{' '}
           or{' '}
-          <Anchor href='https://chrishartgerink.typeform.com/to/VNfDMq'>
+          <Anchor href='https://forms.gle/YrQpmWYdQ7KdMoH18'>
             testing
           </Anchor>
           . Want to financially support us? Become a{' '}
