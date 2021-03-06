647869b1d44fb6ae7029a91f5fa9d796bdef86a2
Chris Hartgerink <chris@libscie.org>
2021-03-06 21:26:51 +0100

    Add initial map example

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -3,57 +3,57 @@ import { TopRow, Title } from '../layout/grid'
 import Loading, { LoadingFlex } from '../loading/loading'
 import MapImage from './map-image'
 
-
 export default ({ p2p }) => {
   const [mapData, setMapData] = useState()
 
   useEffect(() => {
     ;(async () => {
-        const [profiles, contents] = await Promise.all([
-            p2p.listProfiles(),
-            p2p.listContent()
-          ])
-          const graph = {
-            nodes: [
-              ...profiles.map(module => ({
-                url: module.rawJSON.url,
-                type: module.rawJSON.type,
-                subtype: module.rawJSON.subtype
+      const [profiles, contents] = await Promise.all([
+        p2p.listProfiles(),
+        p2p.listContent()
+      ])
+      const graph = {
+        nodes: [
+          ...profiles.map(module => ({
+            id: module.rawJSON.url,
+            type: module.rawJSON.type,
+            subtype: module.rawJSON.subtype
+          })),
+          ...contents.map(module => ({
+            id: `${module.rawJSON.url}+${module.metadata.version}`,
+            type: module.rawJSON.type,
+            subtype: module.rawJSON.subtype
+          }))
+        ],
+        links: [
+          ...profiles
+            .map(profile => [
+              ...profile.rawJSON.contents.map(url => ({
+                source: profile.rawJSON.url,
+                target: `hyper://${url}`
               })),
-              ...contents.map(module => ({
-                url: `${module.rawJSON.url}+${module.metadata.version}`,
-                type: module.rawJSON.type,
-                subtype: module.rawJSON.subtype
+              ...profile.rawJSON.follows.map(url => ({
+                source: profile.rawJSON.url,
+                target: `hyper://${url}`
               }))
-            ],
-            edges: [
-              ...profiles
-                .map(profile => [
-                  ...profile.rawJSON.contents.map(url => ({
-                    source: profile.rawJSON.url,
-                    target: `hyper://${url}`
-                  })),
-                  ...profile.rawJSON.follows.map(url => ({
-                    source: profile.rawJSON.url,
-                    target: `hyper://${url}`
-                  }))
-                ])
-                .flat(),
-              ...contents
-                .map(content => [
-                  ...content.rawJSON.parents.map(url => ({
-                    source: `${content.rawJSON.url}+${content.metadata.version}`,
-                    target: `hyper://${url}`
-                  })),
-                  ...content.rawJSON.authors.map(url => ({
-                    source: `${content.rawJSON.url}+${content.metadata.version}`,
-                    target: `hyper://${url}`
-                  }))
-                ])
-                .flat()
-            ]
-          }
-        setMapData(graph)
+            ])
+            .flat(),
+          ...contents
+            .map(content => [
+              ...content.rawJSON.parents.map(url => ({
+                source: `${content.rawJSON.url}+${content.metadata.version}`,
+                target: `hyper://${url}`
+              })),
+              ...content.rawJSON.authors.map(url => ({
+                source: `${content.rawJSON.url}+${content.metadata.version}`,
+                target: `hyper://${url}`
+              }))
+            ])
+            .flat()
+        ]
+      }
+
+      setMapData(graph)
     })()
   }, [])
 
@@ -62,8 +62,8 @@ export default ({ p2p }) => {
       <TopRow>
         <Title>Map</Title>
       </TopRow>
-        {true ? (
-          <MapImage graph={mapData} />
+      {true ? (
+        <MapImage graph={mapData} />
       ) : (
         <LoadingFlex>
           <Loading />
